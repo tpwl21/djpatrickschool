b943ec31f5f87a3e3b5cdea48101b237d5fef860
tpwl21 <theophane.wallerich@gmail.com>
2026-04-17 05:54:09 +0200

    chore: setup github pages deployment

--- src/constants/tracks.js
+++ src/constants/tracks.js
@@ -16,33 +16,33 @@ export const TRACK_CONFIG = {
     B: { url: null, bpm: 135, complexity: 1, title: "Vitesse à caler", key: "5A" }
   },
   LEVEL_3: {
-    A: { url: "/AUDIO/Trip.mp3", bpm: 122, complexity: 3, title: "Base", key: "4A" },
-    B: { url: "/AUDIO/What_is_music.mp3", bpm: 126, complexity: 3, title: "Rythme fou", key: "8A" }
+    A: { url: "AUDIO/Trip.mp3", bpm: 122, complexity: 3, title: "Base", key: "4A" },
+    B: { url: "AUDIO/What_is_music.mp3", bpm: 126, complexity: 3, title: "Rythme fou", key: "8A" }
   },
   LEVEL_4: {
-    A: { url: "/AUDIO/Estupido.mp3", bpm: 125, complexity: 4, title: "Boucle Ref", key: "11A" },
-    B: { url: "/AUDIO/SCIFI.mp3", bpm: 128, complexity: 4, title: "Boucle à caler", key: "12A" }
+    A: { url: "AUDIO/Estupido.mp3", bpm: 125, complexity: 4, title: "Boucle Ref", key: "11A" },
+    B: { url: "AUDIO/SCIFI.mp3", bpm: 128, complexity: 4, title: "Boucle à caler", key: "12A" }
   },
   LEVEL_5: {
-    A: { url: "/AUDIO/Funkasteroid.mp3", bpm: 126, complexity: 3, title: "Base Clap", key: "4A" },
-    B: { url: "/AUDIO/SuperMarket.mp3", bpm: 130, complexity: 3, title: "Start on 2", key: "8A" }
+    A: { url: "AUDIO/Funkasteroid.mp3", bpm: 126, complexity: 3, title: "Base Clap", key: "4A" },
+    B: { url: "AUDIO/SuperMarket.mp3", bpm: 130, complexity: 3, title: "Start on 2", key: "8A" }
   },
   LEVEL_6: {
-    A: { url: "/AUDIO/Ensaios.mp3", bpm: 130, complexity: 5, title: "Intro", key: "1A" },
-    B: { url: "/AUDIO/SuperMarket.mp3", bpm: 130, complexity: 5, title: "Outro", key: "12A" }
+    A: { url: "AUDIO/Ensaios.mp3", bpm: 130, complexity: 5, title: "Intro", key: "1A" },
+    B: { url: "AUDIO/SuperMarket.mp3", bpm: 130, complexity: 5, title: "Outro", key: "12A" }
   },
   LEVEL_7: {
-    A: { url: "/AUDIO/before_palmos.mp3", bpm: 130, complexity: 4, title: "Boucle Ref", key: "11A" },
-    B: { url: "/AUDIO/Jazzman_Doucheur.mp3", bpm: 128, complexity: 4, title: "Boucle à caler", key: "12A" }
+    A: { url: "AUDIO/before_palmos.mp3", bpm: 130, complexity: 4, title: "Boucle Ref", key: "11A" },
+    B: { url: "AUDIO/Jazzman_Doucheur.mp3", bpm: 128, complexity: 4, title: "Boucle à caler", key: "12A" }
   },
   LEVEL_8: {
-    A: { url: "/AUDIO/Bjork.mp3", bpm: 132, complexity: 4, title: "Boucle Ref", key: "11A" },
-    B: { url: "/AUDIO/Sourire.mp3", bpm: 128, complexity: 4, title: "Boucle à caler", key: "11A" }
+    A: { url: "AUDIO/Bjork.mp3", bpm: 132, complexity: 4, title: "Boucle Ref", key: "11A" },
+    B: { url: "AUDIO/Sourire.mp3", bpm: 128, complexity: 4, title: "Boucle à caler", key: "11A" }
   }
 }; export const HARMONY_POOL = [
-  { url: "/AUDIO/Trip.mp3", bpm: 122, title: "Trip", key: "4A" },
-  { url: "/AUDIO/Tech.mp3", bpm: 127, title: "Tech", key: "8A" },
-  { url: "/AUDIO/SuperMarket.mp3", bpm: 130, title: "Market", key: "11A" },
-  { url: "/AUDIO/SCIFI.mp3", bpm: 128, title: "SciFi", key: "12A" },
-  { url: "/AUDIO/Ensaios.mp3", bpm: 130, title: "Ensaios", key: "1A" },
+  { url: "AUDIO/Trip.mp3", bpm: 122, title: "Trip", key: "4A" },
+  { url: "AUDIO/Tech.mp3", bpm: 127, title: "Tech", key: "8A" },
+  { url: "AUDIO/SuperMarket.mp3", bpm: 130, title: "Market", key: "11A" },
+  { url: "AUDIO/SCIFI.mp3", bpm: 128, title: "SciFi", key: "12A" },
+  { url: "AUDIO/Ensaios.mp3", bpm: 130, title: "Ensaios", key: "1A" },
 ];
